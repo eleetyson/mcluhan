a543dbfc2b4b79f5574f08e49be80921dc162347
Ethan <eleetyson@gmail.com>
2020-11-22 11:33:18 -0500

    begin speech to text transcription feature

---
+++ index.js
@@ -0,0 +1,53 @@
+const button = document.querySelector("button")
+const error = document.getElementById("error")
+const textarea = document.querySelector("textarea")
+
+window.addEventListener("DOMContentLoaded", () => {
+  const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+
+  if (typeof SpeechRecognition === "undefined") {
+    button.remove()
+    error.classList.remove("hidden")
+
+  } else {
+    let listening = false
+    const recognition = new SpeechRecognition()
+
+    const start = () => {
+      recognition.start()
+      toggleListenOnStyling()
+    }
+    const stop = () => {
+      recognition.stop()
+      toggleListenOffStyling()
+    }
+    const onResult = event => {
+      textarea.innerText = ""
+      for (const res of event.results) {
+        const transcription = res[0].transcript
+        textarea.innerText += transcription
+      }
+    }
+    recognition.continuous = true
+    recognition.interimResults = true
+    recognition.addEventListener("result", onResult)
+
+    button.addEventListener("click", () => {
+      listening ? stop() : start()
+      listening = !listening
+    })
+  } // end if-else
+
+}) // end event listener
+
+function toggleListenOnStyling() {
+  button.textContent = "Stop listening"
+  button.classList.remove("btn-dark")
+  button.classList.add("btn-danger")
+}
+
+function toggleListenOffStyling() {
+  button.textContent = "Start listening"
+  button.classList.remove("btn-danger")
+  button.classList.add("btn-dark")
+}
